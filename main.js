7e5c23971a556335e8628eee2d1e0f5d34592462
shimojo-mado <40969845+shimojo-mado@users.noreply.github.com>
2018-07-09 14:57:45 +0900

    js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     let isClipSupport = () => {
         let div = document.createElement('div');
         div.style.cssText = 'clip-path: circle(0 at right 2rem top 1.24rem)';
-        return false && div.style.length;
+        return div.style.length;
     };
     let main = () => {
         let pageNum = 0,
